73157757a77710b5b05e00e9c1297e4a46816d9e
Benjamin Bulis <beni1999@gmx.at>
2025-02-04 23:38:32 +0100

    deleted initial code from vue and working on login component

---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+function click() {
+  alert('Hello')
+}
+</script>
+
+<template>
+  <div class="login-box">
+    <h1>User Verwaltung xben.org</h1>
+    <div class="login-box-section">
+      <p>E-Mail</p>
+      <input type="email" class="input" />
+    </div>
+    <div class="login-box-section">
+      <p>Passwort</p>
+      <input type="password" class="input" />
+    </div>
+    <div class="login-box-section login-box-button">
+      <button @click="click" class="button button-primary">Anmelden</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-box {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  margin-top: 3rem;
+}
+
+.login-box-section {
+  padding: 1rem 0;
+}
+
+button {
+  margin-right: 0.5rem;
+}
+
+input {
+  width: 100%;
+}
+</style>
